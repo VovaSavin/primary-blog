99b96677c18f23b9dd6352382fbb7d3b219778a8
vova_savin <volodimirsavin56@gmail.com>
2021-03-12 21:26:33 +0200

    Добавлена форма фосстановления пароля. Соответствующие шаблоны добавлены

--- custom/settings.py
+++ custom/settings.py
@@ -125,3 +125,5 @@ STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
