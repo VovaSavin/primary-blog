3ce25970e9baedd57276d6ae20fe32b4fa49ab03
vova_savin <volodimirsavin56@gmail.com>
2021-05-24 19:48:38 +0300

    Added heroku allowed host

--- custom/server_settings.py
+++ custom/server_settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get(
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['fast-everglades-38514.herokuapp.com', '127.0.0.1']
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
